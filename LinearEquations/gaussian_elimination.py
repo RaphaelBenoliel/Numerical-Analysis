38382a9794327f9e7f8ccee2713d76acc025dfa6
Daniel Vaknin <danieva3@ac.sce.ac.il>
2024-02-17 23:28:53 +0200

    max norm and condition number

--- LinearEquations/gaussian_elimination.py
+++ LinearEquations/gaussian_elimination.py
@@ -1,3 +1,13 @@
+# Date:
+# Groups: Raphael Benoliel 209946854
+#         Daniel Vaknin 314753161
+#         Maor Hadad
+#         Bar Cohen
+# Git: https://github.com/RaphaelBenoliel/Numerical-Analysis/blob/main/LinearEquations/gaussian_elimination.py
+#
+
+
+
 import numpy as np
 from numpy.linalg import norm, inv
 
