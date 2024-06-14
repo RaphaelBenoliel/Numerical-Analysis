3c39e43cf9457a154e742a7b265d98ae75c7f402
Raphael Benoliel <raphael2gb@gmail.com>
2024-06-14 10:55:43 +0300

    richardson_derivative algorithm

--- richardson_derivative.py
+++ richardson_derivative.py
@@ -1,13 +1,14 @@
+import math
+
+
 def richardson_derivative(f, x, h):
     D_h = (f(x + h) - f(x)) / h
     D_2h = (f(x + 2*h) - f(x)) / (2*h)
     D_richardson = (4 * D_h - D_2h) / 3
     return D_richardson
 
+
 if __name__ == '__main__':
-    import math
-
-
     def f(x):
         return math.sin(x)
 
